dd48fd66b748b560ffca16004bc02571599549ed
iTomaaaHD <toma98@hotmail.co.uk>
2015-08-18 12:00:53 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,10 +1,14 @@
 from tealight.logo import move, turn
 
-def draw(gridlength):
+def drawgrid(gridlength):
   for i in range(0,4):
     move(gridlength)
     turn(90)
     move(gridlength)
   
+def drawsquares(squareslength):
+  move(10)
+  turn(-90)
+  move(100)
 
-draw(100)
+drawgrid(100)
