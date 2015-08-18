b4888c700d61cd2d768aa8b8474fc6f11c0fdad3
iTomaaaHD <toma98@hotmail.co.uk>
2015-08-18 12:03:19 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -6,11 +6,11 @@ def drawgrid(gridlength):
     turn(90)
   
 def drawsquares(squareslength):
-  for i in range(0,10)
-  turn(90)
-  move(squareslength)
-  turn(-90)
-  move(100)
+  for i in range(0,10):
+    turn(90)
+    move(squareslength)
+    turn(-90)
+    move(100)
 
 drawgrid(100)
 drawsquares(10)
