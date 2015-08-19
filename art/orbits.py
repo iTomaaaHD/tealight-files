583bc2daa3bff97b777a6a18913568ce4c41d3b1
iTomaaaHD <toma98@hotmail.co.uk>
2015-08-19 09:51:30 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -10,12 +10,12 @@ ax = 0
 ay = 0
 
 power = 0.3
-
+gravity = 0.1
+ay = ay + gravity
 
 def handle_keydown(key):
   global ax, ay
-  gravity = 0.1
-  ay = ay + gravity
+  
 
   if key == "left":
     ax = -power
@@ -28,8 +28,6 @@ def handle_keydown(key):
 
 def handle_keyup(key):
   global ax, ay
-  gravity = 0.1
-  ay = ay + gravity
 
   if key == "left" or key == "right":
     ax = 0
