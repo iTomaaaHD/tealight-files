bc17618a1e7ec76dd04e4e6ac3198b50c4d31b33
iTomaaaHD <toma98@hotmail.co.uk>
2015-08-19 09:38:21 +0100

    Running art/stars.py

--- art/stars.py
+++ art/stars.py
@@ -1,6 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-from math import sin, cos, pi,
+from math import sin, cos, pi
 
 def star(x, y, c, size, spines):
   
