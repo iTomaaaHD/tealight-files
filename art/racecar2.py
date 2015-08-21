e3966cf4423393260af75d444a770212cae68ebf
iTomaaaHD <toma98@hotmail.co.uk>
2015-08-21 09:30:31 +0100

    Running art/racecar2.py

--- art/racecar2.py
+++ art/racecar2.py
@@ -3,6 +3,7 @@ from math import *
 carcolour = "red"
 y = screen_height/2
 x = screen_width/2
+background("http://www.electricdreams.com/slotcar-news/wp-content/uploads/2010/10/10x18trioval-1b.jpg")
 
 car = [(x,y),(x+50,y),(x-40,y-25),(x-40,y+25),(x-40,y-25)
        ,(x,y),(x-40,y+25),(x,y),(x-40,y+25),(x+50,y),
